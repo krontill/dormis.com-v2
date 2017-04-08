130116ea2e087c60fb38372119745c31185fec64
krontill <till.work@gmail.com>
2017-04-08 16:33:12 +0300

    Fix daterangepicker adaptive.

--- markup/components/scripts/scrolling-close.js
+++ markup/components/scripts/scrolling-close.js
@@ -1,20 +1,22 @@
 $(window).on('load', function () {
     //Load для того чтобы скрипт не срабатывал при первоначальном скролле при загрузке страницы, когда скролл сработал, а select2 еще нет.
     $(window).scroll(function (e) {
-        if($(e.target)[0].parentElement === null) {
-            $('.js-select2-city').select2('close');
-        }
-        $('.js-currency_user').select2('close');
-        $('.js-lang_user').select2('close');
-        if ($('.list-guests').is(':visible')) {
-            $('.select2-guestnumber-click').removeClass('select2-container--open');
-            $('.list-guests').hide();
-        }
-        if ($('#arrival-date').data('daterangepicker').isShowing) {
-            $('#arrival-date').data('daterangepicker').hide();
-        }
-        if ($('#departure-date').data('daterangepicker').isShowing) {
-            $('#departure-date').data('daterangepicker').hide();
+        if($(window).width() > 991){
+            if($(e.target)[0].parentElement === null) {
+                $('.js-select2-city').select2('close');
+            }
+            $('.js-currency_user').select2('close');
+            $('.js-lang_user').select2('close');
+            if ($('.list-guests').is(':visible')) {
+                $('.select2-guestnumber-click').removeClass('select2-container--open');
+                $('.list-guests').hide();
+            }
+            if ($('#arrival-date').data('daterangepicker').isShowing) {
+                $('#arrival-date').data('daterangepicker').hide();
+            }
+            if ($('#departure-date').data('daterangepicker').isShowing) {
+                $('#departure-date').data('daterangepicker').hide();
+            }
         }
     });
 });
